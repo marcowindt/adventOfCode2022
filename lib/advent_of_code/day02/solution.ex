a50c72444f49a784a17d648a0b602e9b99065638
Marco Dijkslag <9336858+marcowindt@users.noreply.github.com>
2022-12-02 10:40:49 +0100

    day 2 clean up

--- lib/advent_of_code/day02/solution.ex
+++ lib/advent_of_code/day02/solution.ex
@@ -9,18 +9,12 @@ defmodule AdventOfCode.Day02.Solution do
 
   def part1(contents) do
     scores = %{
-      ["A", "X"] => 1 + 3,
-      ["A", "Y"] => 2 + 6,
-      ["A", "Z"] => 3 + 0,
-      ["B", "X"] => 1 + 0,
-      ["B", "Y"] => 2 + 3,
-      ["B", "Z"] => 3 + 6,
-      ["C", "X"] => 1 + 6,
-      ["C", "Y"] => 2 + 0,
-      ["C", "Z"] => 3 + 3
+      "A Y" => 2 + 6, "A X" => 1 + 3, "A Z" => 3 + 0,
+      "B X" => 1 + 0, "B Y" => 2 + 3, "B Z" => 3 + 6,
+      "C X" => 1 + 6, "C Y" => 2 + 0, "C Z" => 3 + 3
     }
 
-    IO.puts("1: #{contents |> Enum.reduce(0, & scores[String.split(&1, " ")] + &2)}")
+    IO.puts("1: #{contents |> Enum.reduce(0, & scores[&1] + &2)}")
   end
 
   @doc """
@@ -34,18 +28,12 @@ defmodule AdventOfCode.Day02.Solution do
   """
   def part2(contents) do
     decide = %{
-      "A" => %{"X" => 3 + 0, "Y"=> 1 + 3, "Z" => 2 + 6},
-      "B" => %{"X" => 1 + 0, "Y"=> 2 + 3, "Z" => 3 + 6},
-      "C" => %{"X" => 2 + 0, "Y"=> 3 + 3, "Z" => 1 + 6}
+      "A X" => 3 + 0, "A Y" => 1 + 3, "A Z" => 2 + 6,
+      "B X" => 1 + 0, "B Y" => 2 + 3, "B Z" => 3 + 6,
+      "C X" => 2 + 0, "C Y" => 3 + 3, "C Z" => 1 + 6
     }
 
-    score =
-      contents
-      |> Enum.reduce(0, fn x, y ->
-        [left, right] = String.split(x, " ")
-        decide[left][right] + y
-      end)
-    IO.puts("2: #{score}")
+    IO.puts("2: #{contents |> Enum.reduce(0, & decide[&1] + &2)}")
   end
 
 end
