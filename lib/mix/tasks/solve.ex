0ff6a150fc2f82ef6a4c554454f62b772dbfac5f
Marco Dijkslag <9336858+marcowindt@users.noreply.github.com>
2022-12-25 19:44:37 +0100

    mix task to solve all after each other

--- lib/mix/tasks/solve.ex
+++ lib/mix/tasks/solve.ex
@@ -1,6 +1,18 @@
 defmodule Mix.Tasks.Solve do
   use Mix.Task
 
+  def run(["--all"]) do
+    {time, :ok} = :timer.tc(fn ->
+      1..25
+      |> Enum.each(fn day ->
+        IO.puts("====== Day #{String.pad_leading("#{day}", 2, "0")} ======")
+        run(["#{day}"])
+      end)
+    end)
+
+    IO.puts("took #{time / 1000} ms")
+  end
+
   def run([day]) do
     module = String.to_existing_atom("Elixir.AdventOfCode.Day#{String.pad_leading(day, 2, "0")}.Solution")
     module.run()
